2cfecdfc21a622c9c755753243b5658371129d8f
piccoloatlantelinguistico <64254040+piccoloatlantelinguistico@users.noreply.github.com>
2020-04-29 20:33:59 +0200

    Add files via upload

---
+++ test/js/1-colori-test.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	
+	$("#thesis").attr({href:"../thesis.html"});
+	
+	
+	$("#exit").attr({href:"../pagina2.html"});
+	
+	$("#back").attr({href:"javascript:window.history.back();"});
+	
+	
+	$("#bottone-fondo-pagina-0").attr({href:"1-colori-test1.html"});
+	
+	$("#bottone-fondo-pagina-1").attr({href:"1-colori-test2.html"});
+	
+	$("#bottone-fondo-pagina-2").attr({href:"1-colori-test3.html"});
+	
+	$("#bottone-fondo-pagina-3").attr({href:"1-colori-test4.html"});
+	
+	$("#bottone-fondo-pagina-4").attr({href:"1-colori-test5.html"});
+	
+	$("#bottone-fondo-pagina-5").attr({href:"1-colori-test6.html"});
+	
+	$("#bottone-fondo-pagina-6").attr({href:"1-colori-test7.html"});
+	
+	$("#bottone-fondo-pagina-7").attr({href:"1-colori-test8.html"});
+	
+	$("#bottone-fondo-pagina-8").attr({href:"1-colori-test9.html"});
+	
+	$("#bottone-fondo-pagina-9").attr({href:"1-colori-test10.html"});
+	
+	$("#bottone-fondo-pagina-10").attr({href:"1-colori-test11.html"});
+	
+	$("#bottone-fondo-pagina-11").attr({href:"../pagina2.html"});
+	
+});
